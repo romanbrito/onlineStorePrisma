8d768d027c0cf80b2482cd286e79796f1ae779ba
Roman Brito <romanbrito1@gmail.com>
2018-12-19 10:40:51 -0600

    signin mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -75,6 +75,28 @@ const Mutations = {
     });
     // return user to the browser
     return user;
+  },
+  async signin(parent, args, ctx, info) {
+    const {email, password} = args;
+    // 1. check if there is a user with that email
+    const user = await ctx.db.query.user({where: {email}});
+    if(!user) {
+      throw new Error(`No such user found for Email ${email}`);
+    }
+    // 2. check if the password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if(!valid) {
+      throw new Error('Invalid Password!');
+    }
+    // 3. generate the JWT token
+    const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
+    // 4. set the cookie with the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // 5. Return the user
+    return user;
   }
 };
 
