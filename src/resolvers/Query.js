d9b195b0623567fa320d51b231bd94317543eee7
Roman Brito <romanbrito1@gmail.com>
2018-12-30 16:22:15 -0600

    single order page

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -26,9 +26,30 @@ const Query = {
 
     // 3. query all the users
     return ctx.db.query.users({}, info); //info contains all the graphql query that contain the fields
-                                    // we are requesting from front end
+    // we are requesting from front end
 
-  }
+  },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You arent logged in!');
+    }
+    // 2. Query the current order
+    const order = await ctx.db.query.order(
+      {
+        where: {id: args.id},
+      },
+      info
+    );
+    // 3. Check if the have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+    if (!ownsOrder && !hasPermissionToSeeOrder) {
+      throw new Error('You cant see this buddd');
+    }
+    // 4. Return the order
+    return order;
+  },
 };
 
 module.exports = Query;
